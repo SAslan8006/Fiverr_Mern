cec5d841a2c9601f4168b12c597c40bc0083d66c
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-04-13 21:28:43 +0300

    bcryt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 
 export const register = async (req, res, next) => {
   try {
-    const hash = bcrypt.hashSync(req.body.password, 5);
+    const hash = bcrypt.hashSync(req.body.password, 10);
     const newUser = new User({
       ...req.body,
       password: hash,
